bc76c03126293a0e5a588aae921556e129643671
changu-Lee <cksrn0003@naver.com>
2024-05-08 20:10:09 +0900

    240508

--- step_up_pratice/level1_variable.c
+++ step_up_pratice/level1_variable.c
@@ -1,11 +1,34 @@
 #include<stdio.h>
 #define _CRT_SECURE_NO_WARNING
 
-int main(){
+int main1(){
     float a;
 
     scanf("%f", &a);
     printf("%.1f", a);
+
+    return 0;
+}
+
+int main2(){
+    double pi=3.14;
+    int r;
+    double cir;
+
+    scanf("%d", &r);
+    cir=2*r*pi;
+    printf("%f", cir);
+
+    return 0;
+}
+
+int main3(){
+    double area, pi=3.14;
+    int r;
+
+    scanf("%d", &r);
+    area=pi*r*r;
+    printf("%.2f", area);
 
     return 0;
 }
