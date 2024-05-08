bba15b43897fb5b29da6fe828d0c698f7d8e8557
changu-Lee <cksrn0003@naver.com>
2024-05-08 20:14:00 +0900

    240508

--- step_up_pratice/level1_variable.c
+++ step_up_pratice/level1_variable.c
@@ -1,7 +1,9 @@
+//충북대 소중단 step_up C프로그래밍 기초 LEVEL 1
+//변수의 선언과 사용
 #include<stdio.h>
 #define _CRT_SECURE_NO_WARNING
 
-int main1(){
+int main1(){            //입력받은 소수 출력하기
     float a;
 
     scanf("%f", &a);
@@ -10,7 +12,7 @@ int main1(){
     return 0;
 }
 
-int main2(){
+int main2(){            //입력받은 정수의 길이를 반지름으로 갖는 원의 둘레 구하기
     double pi=3.14;
     int r;
     double cir;
@@ -22,7 +24,7 @@ int main2(){
     return 0;
 }
 
-int main3(){
+int main3(){            //입력받은 정수의 길이를 반지름으로 갖는 원의 넓이 구하기
     double area, pi=3.14;
     int r;
 
